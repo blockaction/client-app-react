e47aae4aee03dd2f436776b76539a83c9878f353
Sarik Dhungel <sareek007@gmail.com>
2020-07-08 13:01:29 +0545

    constants add

---
+++ utils/constants.js
@@ -0,0 +1,14 @@
+export const URL = "";
+export const imgURL = "https://dev-bitsbeat-s3.s3.amazonaws.com/";
+export const apiUrl = `https://altona.blockaction.io/api/`;
+export const adminApiUrl = `https://altona.blockaction.io/adminapi/`;
+export const tempUrl = `https://api.prylabs.net/`;
+export const RECAPTCHA_SITE_KEY = "6LebxsYUAAAAAO-KFQMh7O0cDrHDkYLThPxIafJj";
+export const DOCUMENT_URL_UPDATE =
+  process.env.NODE_ENV === "production"
+    ? "https://dev-bitsbeat-s3.s3.amazonaws.com/"
+    : "https://dev-bitsbeat-s3.s3.amazonaws.com/";
+export const websiteTitle = "Blockaction | Ethereum 2.0 Beacon Chain Explorer";
+export const websiteDescription = "ETH";
+export const websiteImage = "http://blockaction.io/static/banner/display_img.png";
+export const websiteKeywords = "ETH";
